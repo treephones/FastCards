4ceb4b1b19a6fbf3ec48a3ef5ab0c1a7708c6c36
Moez B <moezbajwa@hotmail.com>
2021-11-29 18:31:19 -0500

    added package.json

--- fastcards.js
+++ fastcards.js
@@ -1,6 +1,9 @@
 //requirements
 const express = require('express');
+require('dotenv').config();
 
+//initialization
+const port = process.env.PORT | 3000;
 let app = express();
 
 app.use(express.static("public"));
@@ -10,3 +13,11 @@ app.use(express.json());
 app.get('/statics/:page/:file', (req, res) => {
     res.sendFile(__dirname + `/${req.params.page}` + `/${req.params.file}`);
 });
+
+//routes
+app.get(['/', '/home'], (req, res) => {
+    res.sendFile(`${__dirname}/statics/home/index.html`);
+});
+
+app.listen(port);
+console.log(`FastCards has started and is listening on port ${port}.`);
